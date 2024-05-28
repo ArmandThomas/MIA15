89fffc06e7adc01bf12f0a75f57865d2ba87624d
sofiane <s.msatfa@ecole-ipssi.net>
2024-05-28 16:12:49 +0200

    migration vers queries api de drizzle

--- apps/api/src/database/schema/athletes.ts
+++ apps/api/src/database/schema/athletes.ts
@@ -1,4 +1,4 @@
-import { mysqlTable, varchar, serial, smallint, text } from "drizzle-orm/mysql-core";
+import { mysqlTable, varchar, serial, smallint, text, bigint } from "drizzle-orm/mysql-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { relations } from "drizzle-orm";
 import { countries } from "./countries.js";
@@ -6,10 +6,12 @@ import { countries } from "./countries.js";
 export const athletes = mysqlTable("athletes", {
   id: serial("id").primaryKey(),
   fullName: varchar("full_name", { length: 256 }).notNull(),
-  firstEdition: serial("first_edition").notNull(),
+  firstEdition: bigint("first_edition", { mode: "number", unsigned: true }).notNull(),
   birthYear: smallint("birth_year").notNull(),
   bio: text("bio"),
-  idCountry: serial("id_country").notNull().references(() => countries.id),
+  idCountry: bigint("id_country", { mode: "number", unsigned: true })
+    .notNull()
+    .references(() => countries.id),
 });
 
 export const athletesRelations = relations(athletes, ({ one }) => ({
--- apps/api/src/services/country-service.ts
+++ apps/api/src/services/country-service.ts
@@ -1,17 +1,16 @@
 import { db } from "@/database/client.js";
-import { eq } from "drizzle-orm";
-import { countries } from "@/database/schema/countries.js";
 import type { Country } from "@/database/schema/countries.js";
 
 async function findOne(id: number): Promise<Country | null> {
-  const [country] = await db.select().from(countries).where(eq(countries.id, id));
+  const country = await db.query.countries.findFirst({ 
+    where: (countries, { eq }) => eq(countries.id, id) 
+  });
 
   return country || null;
 }
 
 async function findAll(): Promise<Country[]> {
-  const allCountries = await db.select().from(countries);
-
+  const allCountries = await db.query.countries.findMany();
   return allCountries;
 }
 
